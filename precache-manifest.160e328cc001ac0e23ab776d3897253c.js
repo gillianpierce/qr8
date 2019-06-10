cb96b58071ad9038fb54876f180d057bad4a2d87
Gillian Pierce <gmpierce@ualberta.ca>
2019-06-10 11:36:00 -0400

    Updates

---
+++ precache-manifest.160e328cc001ac0e23ab776d3897253c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6790806430d398f8a6ecf69fb335d8fc",
+    "url": "/qr8/index.html"
+  },
+  {
+    "revision": "e357577df228d049fe13",
+    "url": "/qr8/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "fb3efae132716450b9d7",
+    "url": "/qr8/static/js/2.6e4d544d.chunk.js"
+  },
+  {
+    "revision": "e357577df228d049fe13",
+    "url": "/qr8/static/js/main.27daf238.chunk.js"
+  },
+  {
+    "revision": "2914435c9a890a519b32",
+    "url": "/qr8/static/js/runtime~main.867e866a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/qr8/static/media/logo.5d5d9eef.svg"
+  }
+]);
